6c50374384ed804e310a8e6b319396119cc68321
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2025-06-10 14:39:06 +0100

    Remove PaymentCallback component and its route from App for code cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import WhatsAppButton from "./components/WhatsAppButton";
 import { Routes, Route } from "react-router-dom";
-import PaymentCallback from "./pages/PaymentCallback";
 import HomeScreen from "./screens/HomeScreen";
 
 function App() {
@@ -12,7 +11,6 @@ function App() {
 			<main className="main" style={{ minHeight: "100vh" }}>
 				<Routes>
 					<Route path="/" element={<HomeScreen />} />
-					<Route path="/payment-callback" element={<PaymentCallback />} />
 				</Routes>
 			</main>
 			<Footer />
